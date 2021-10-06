dc42645f60793e9169668a4eead9d1c1adeea073
Ying Stokes <yingstokes@gmail.com>
2021-10-06 23:37:36 +1030

    cleaned up cli options

--- testing-cli/src/main.rs
+++ testing-cli/src/main.rs
@@ -28,25 +28,33 @@ fn test_smart_contract(client: &RpcClient) {
     // program id to send instructions to
 
     let prog_id = Pubkey::from_str(&env::var("FLU_PROGRAM_ID").unwrap()).unwrap();
+    let key_bytes = std::fs::read_to_string(&env::var("SOLANA_ID_PATH").unwrap()).unwrap();
     let command = env::args().nth(1).unwrap();
-    if command == "help" {
-        println!("[wrap/unwrap] base_token_id fluidity_token_id base_token_account fluidity_token_account");
-        return
-    }
 
     // get recent blockhash
     let (recent_blockhash, _) = client.get_recent_blockhash().unwrap();
 
     // create account to pay for everything
     // here i'm using the default account for my test validator, but that won't work on anything except my system.
-    let key_bytes = std::fs::read_to_string(&env::var("SOLANA_ID_PATH").unwrap()).unwrap();
     let payer = Keypair::from_bytes(&(serde_json::from_str::<Vec<u8>>(&key_bytes).unwrap())).unwrap();
-    println!("{}", payer.pubkey());
+    println!("payer pubkey: {}", payer.pubkey());
 
     // derive address of mint account
     let mint_account_seed = b"FLU: MINT ACCOUNT";
-    let (pool_pubkey, bump_seed) = Pubkey::find_program_address(&[mint_account_seed], &prog_id);
-    println!("{}, {}", pool_pubkey, bump_seed);
+    let (pda_pubkey, bump_seed) = Pubkey::find_program_address(&[mint_account_seed], &prog_id);
+
+    match command.as_str() {
+        "help" => {
+            println!("[wrap/unwrap] base_token_id fluidity_token_id base_token_account fluidity_token_account");
+            return
+        }
+        "printpdakey" => {
+            println!("pda pubkey: {}", pda_pubkey);
+            println!("bump seed: {}", bump_seed);
+            return
+        }
+        _ => {}
+    };
 
     // select and create instruction
     let inst = match command.as_str() {
@@ -56,7 +64,7 @@ fn test_smart_contract(client: &RpcClient) {
             let fluidity_token_id = Pubkey::from_str(&env::args().nth(3).unwrap()).unwrap();
             let base_token_account = Pubkey::from_str(&env::args().nth(4).unwrap()).unwrap();
             let fluidity_token_account = Pubkey::from_str(&env::args().nth(5).unwrap()).unwrap();
-            let pool_token_pubkey = spl_associated_token_account::get_associated_token_address(&pool_pubkey, &base_token_id);
+            let pda_token_pubkey = spl_associated_token_account::get_associated_token_address(&pda_pubkey, &base_token_id);
             Instruction::new_with_borsh(
                 prog_id,
                 &FluidityInstruction::Wrap(amount),
@@ -64,8 +72,8 @@ fn test_smart_contract(client: &RpcClient) {
                     AccountMeta::new_readonly(spl_token::ID, false),
                     AccountMeta::new(base_token_id, false),
                     AccountMeta::new(fluidity_token_id, false),
-                    AccountMeta::new(pool_pubkey, false),
-                    AccountMeta::new(pool_token_pubkey, false),
+                    AccountMeta::new(pda_pubkey, false),
+                    AccountMeta::new(pda_token_pubkey, false),
                     AccountMeta::new(payer.pubkey(), true),
                     AccountMeta::new(base_token_account, false),
                     AccountMeta::new(fluidity_token_account, false),
@@ -78,15 +86,15 @@ fn test_smart_contract(client: &RpcClient) {
             let fluidity_token_id = Pubkey::from_str(&env::args().nth(3).unwrap()).unwrap();
             let base_token_account = Pubkey::from_str(&env::args().nth(4).unwrap()).unwrap();
             let fluidity_token_account = Pubkey::from_str(&env::args().nth(5).unwrap()).unwrap();
-            let pool_token_pubkey = spl_associated_token_account::get_associated_token_address(&pool_pubkey, &base_token_id);
+            let pda_token_pubkey = spl_associated_token_account::get_associated_token_address(&pda_pubkey, &base_token_id);
             Instruction::new_with_borsh(
                 prog_id,
                 &FluidityInstruction::Unwrap(amount),
                 vec![AccountMeta::new_readonly(spl_token::ID, false),
                     AccountMeta::new(base_token_id, false),
                     AccountMeta::new(fluidity_token_id, false),
-                    AccountMeta::new(pool_pubkey, false),
-                    AccountMeta::new(pool_token_pubkey, false),
+                    AccountMeta::new(pda_pubkey, false),
+                    AccountMeta::new(pda_token_pubkey, false),
                     AccountMeta::new(payer.pubkey(), true),
                     AccountMeta::new(base_token_account, false),
                     AccountMeta::new(fluidity_token_account, false),
@@ -102,7 +110,7 @@ fn test_smart_contract(client: &RpcClient) {
                 &mint_pk,
             )
         }
-        _ => panic!("please provide a valid command (wrap, unwrap, createacc)")
+        _ => panic!("please provide a valid command (help, wrap, unwrap, createacc, printpdakey)")
     };
 
     // create and send txn to program
